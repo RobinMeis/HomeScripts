2a310bd47d350ee9e9b23cb7fa5918cab23bb10b
Robin Meis <robin@meis.space>
2017-12-11 01:17:13 +0100

    Fixed cronjobs

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 #This script is gonnas to be called by a cronjob and calls the home automation scripts
+
+cd "$(dirname "$0")" #Set working directory (important for running cronjobs)
 
 python3 aseag2mqtt.py
